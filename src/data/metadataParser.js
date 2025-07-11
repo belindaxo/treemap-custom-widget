540e457900705d1f4aac5a581b79629cde8dcbed
Belinda <bmdebruyn@gmail.com>
2025-07-11 14:24:43 -0600

    first commit

---
+++ src/data/metadataParser.js
@@ -0,0 +1,20 @@
+/**
+ * Parses metadata into structured dimensions and measures.
+ * @param {Object} metadata - The metadata object from SAC data binding.
+ * @returns {Object} An object containing parsed dimensions, measures, and their maps.
+ */
+export function parseMetadata(metadata) {
+    const { dimensions: dimensionsMap, mainStructureMembers: measuresMap } = metadata;
+    const dimensions = [];
+    for (const key in dimensionsMap) {
+        const dimension = dimensionsMap[key];
+        dimensions.push({ key, ...dimension });
+    }
+
+    const measures = [];
+    for (const key in measuresMap) {
+        const measure = measuresMap[key];
+        measures.push({ key, ...measure });
+    }
+    return { dimensions, measures, dimensionsMap, measuresMap };
+}
