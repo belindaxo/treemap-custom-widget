bad3e0bfdc87d87e93c8b18690cf1dbb0a03d30e
Belinda <bmdebruyn@gmail.com>
2025-07-18 15:16:14 -0600

    debug

--- src/index.js
+++ src/index.js
@@ -142,7 +142,7 @@ import { formatTooltip } from './formatting/tooltipformatter';
                     reductionFactor: 10
                 },
                 dataLabels: {
-                    enabled: true, 
+                    enabled: false, 
                     allowOverlap: true
                 },
                 data: seriesData,
